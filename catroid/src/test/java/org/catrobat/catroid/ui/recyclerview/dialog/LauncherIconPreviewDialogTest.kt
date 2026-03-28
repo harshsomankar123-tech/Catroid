545daef8b76509c616f270d94f102e26441210fd
harshsomankar123-tech <harshsomankar123@gmail.com>
2026-03-28 06:32:00 +0530

    tests: Add full UI interaction flow for LauncherIconPreviewDialog

--- catroid/src/test/java/org/catrobat/catroid/ui/recyclerview/dialog/LauncherIconPreviewDialogTest.kt
+++ catroid/src/test/java/org/catrobat/catroid/ui/recyclerview/dialog/LauncherIconPreviewDialogTest.kt
@@ -2,31 +2,30 @@ package org.catrobat.catroid.ui.recyclerview.dialog
 
 import android.graphics.Bitmap
 import android.os.Build
+import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.imageview.ShapeableImageView
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.unmockkAll
+import io.mockk.*
 import org.catrobat.catroid.R
 import org.catrobat.catroid.common.ProjectData
 import org.catrobat.catroid.ui.ProjectLauncherIconProvider
+import org.catrobat.catroid.ui.recyclerview.fragment.ProjectListFragment
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowLog
+import org.robolectric.shadows.ShadowLooper
 import org.robolectric.util.ReflectionHelpers
 import java.io.File
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import org.robolectric.Robolectric
-import org.robolectric.shadows.ShadowLooper
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.P])
@@ -40,7 +39,6 @@ class LauncherIconPreviewDialogTest {
     fun setUp() {
         ShadowLog.stream = System.out
         projectData = ProjectData(projectName, projectDir, 0.9, false)
-        
         mockkStatic(ProjectLauncherIconProvider::class)
     }
 
@@ -50,29 +48,51 @@ class LauncherIconPreviewDialogTest {
     }
 
     @Test
-    fun testDialogViewsArePopulated() {
+    fun testFullInteractionFlow() {
         val mockBitmap = mockk<Bitmap>(relaxed = true)
-        
-        io.mockk.mockkConstructor(ProjectLauncherIconProvider::class)
+        mockkConstructor(ProjectLauncherIconProvider::class)
         every { anyConstructed<ProjectLauncherIconProvider>().getLauncherIcon(any()) } returns mockBitmap
 
         val activityController = Robolectric.buildActivity(FragmentActivity::class.java)
         val activity = activityController.create().start().resume().get()
-        val fragment = LauncherIconPreviewDialog.newInstance(projectData)
         
-        fragment.show(activity.supportFragmentManager, "launcher_icon_preview")
+        // Setup ProjectListFragment
+        val projectListFragment = ProjectListFragment()
+        activity.supportFragmentManager.beginTransaction()
+            .add(android.R.id.content, projectListFragment)
+            .commitNow()
+        
         ShadowLooper.idleMainLooper()
 
-        val rootView = fragment.view ?: throw IllegalStateException("Fragment view should not be null")
-        val projectNameView = rootView.findViewById<TextView>(R.id.project_name_view)
-        val iconView = rootView.findViewById<ShapeableImageView>(R.id.launcher_icon_view)
-        val okButton = rootView.findViewById<MaterialButton>(R.id.ok_button)
+        // Simulate opening the settings menu and clicking 'Launcher icon preview'
+        val mockView = View(activity)
+        projectListFragment.onSettingsClick(projectData, mockView)
+        ShadowLooper.idleMainLooper()
 
-        assertNotNull("Project Name View should not be null", projectNameView)
-        assertNotNull("Icon View should not be null", iconView)
-        assertNotNull("OK Button should not be null", okButton)
+        // Since we can't easily click a real PopupMenu in Robolectric without complexity,
+        // we directly call showLauncherIconPreviewDialog which is what the menu click does.
+        ReflectionHelpers.callInstanceMethod<Any>(
+            projectListFragment, 
+            "showLauncherIconPreviewDialog", 
+            ReflectionHelpers.ClassParameter(ProjectData::class.java, projectData)
+        )
+        ShadowLooper.idleMainLooper()
 
+        // Assert dialog is shown
+        val dialogFragment = projectListFragment.childFragmentManager.findFragmentByTag("LauncherIconPreviewDialog") 
+            as? LauncherIconPreviewDialog
+        assertNotNull("Dialog should be shown", dialogFragment)
+
+        // Verify dialog contents
+        val dialogView = dialogFragment!!.requireView()
+        val projectNameView = dialogView.findViewById<TextView>(R.id.project_name_view)
         assertEquals(projectName, projectNameView?.text.toString())
-        assertEquals(activity.getString(android.R.string.ok), okButton?.text.toString())
+
+        // Click OK button and verify dismissal
+        val okButton = dialogView.findViewById<MaterialButton>(R.id.ok_button)
+        okButton?.performClick()
+        ShadowLooper.idleMainLooper()
+
+        assertNull("Dialog should be dismissed", projectListFragment.childFragmentManager.findFragmentByTag("LauncherIconPreviewDialog"))
     }
 }
